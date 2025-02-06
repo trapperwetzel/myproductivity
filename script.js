0e4c8284a38e1a4e6e60ba651a015e05cd0943bb
t coding <146040255+trapperwetzel@users.noreply.github.com>
2025-02-06 12:33:26 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("task-container");
+
+function addTask(){
+    const task = inputBox.value.trim();
+
+    if(!task) {
+        alert("Please write down a task");
+        return; 
+    }
+
+    
+
+
+
+}
