caf77d6b4c7af287229c67cb42e9185fce48e32e
t_coding <trapperwetzel@gmail.com>
2025-02-17 20:52:56 -0600

    refactor javascript to make it save the goals even if you refresh the page

--- goallist.js
+++ goallist.js
@@ -1,6 +1,9 @@
 const goalInput = document.getElementById("goal-box");
 const goalList = document.getElementById("goal-list");
 
+const goals = JSON.parse(localStorage.getItem('goals')) || [];
+
+goals.forEach(goal => createGoalElement(goal));
 
 goalInput.addEventListener("keydown",function(event){
 
@@ -15,19 +18,56 @@ goalInput.addEventListener("keydown",function(event){
 function addGoal(){
     const goal = goalInput.value.trim();
 
-    const li = document.createElement("li");
+    goals.push(goal);
 
-    li.innerHTML = `
-        <div id = "goalitem">
-          <span contenteditable = "true">${goal}</span>
+    createGoalElement(goal);
+    saveGoals();
+
+    goalInput.value ="";
+}
+
+function createGoalElement(goal){
+   
+
+    const listitem = document.createElement("li");
+
+    listitem.innerHTML = `
+        <div class = "goalitem">
+          <span class = "goal" contenteditable = "true">${goal}</span>
         </div>
+        <span class = "goaldeletebutton">X</span>
     `;
 
-    // Append task to the list
-    listContainer.appendChild(li);
-    goalInput.value = "";
+    document.getElementById("goal-list").appendChild(listitem);
 
-    
+    listitem.querySelector(".goaldeletebutton").addEventListener("click",function(){
+        listitem.remove();
 
-    
+        const index = goals.indexOf(goal);
+        if(index >-1){
+            goals.splice(index,1);
+        }
+        saveGoals();
+
+    });
+    listitem.querySelector(".goal").addEventListener("input",function(){
+        saveGoals();
+
+    });
 }
+
+function saveGoals() {
+    const goalElements = document.querySelectorAll("#goal-list li .goal");
+    const updatedGoals = [];
+    goalElements.forEach(el => updatedGoals.push(el.textContent));
+    localStorage.setItem("goals", JSON.stringify(updatedGoals));
+}
+
+
+    
+
+    
+
+    
+
+    
